889c447bd02bc0811e8b27936c5acdddeb42b906
PRABHAKAR KUMAR <prabhakarkumar2004april@gmail.com>
2025-01-26 12:36:27 +0530

    new environment scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,13 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    private SoundManager soundManager; 
+
+    private void Start()
+    {
+        soundManager = FindObjectOfType<SoundManager>();
+    }
+
     public void PlayGame()
     {
+        if (soundManager != null)
+        {
+            soundManager.PlayButtonClickSound();
+        }
+        
         SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
     {
+        if (soundManager != null)
+        {
+            soundManager.PlayButtonClickSound();
+        }
         Application.Quit();
     }
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -9,14 +9,26 @@ public class SoundManager : MonoBehaviour
     [SerializeField] Slider sfxVolumeSlider;
     
     [Header("Audio Sources")]
-    public AudioSource backgroundMusic; // Audio source for background music
+    public AudioSource backgroundMusic;
     public AudioSource sfxSource;
+    
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip backgroundMusicClip;
+    [SerializeField] private AudioClip buttonClickClip;
     
     private const string masterVolumeKey = "masterVolume";
     private const string sfxVolumeKey = "sfxVolume";
 
     private void Start()
     {
+        
+        if (backgroundMusic != null && backgroundMusicClip != null)
+        {
+            backgroundMusic.clip = backgroundMusicClip;
+            backgroundMusic.loop = true; 
+            backgroundMusic.Play();
+        }
+        
         float savedMasterVolume = PlayerPrefs.GetFloat(masterVolumeKey, 1f);
         float savedSfxVolume = PlayerPrefs.GetFloat(sfxVolumeKey, 1f);
         
@@ -50,5 +62,13 @@ public class SoundManager : MonoBehaviour
     
         PlayerPrefs.SetFloat(sfxVolumeKey, volume);
         PlayerPrefs.Save();
+    }
+    
+    public void PlayButtonClickSound()
+    {
+        if (sfxSource != null && buttonClickClip != null)
+        {
+            sfxSource.PlayOneShot(buttonClickClip);
+        }
     }
 }
